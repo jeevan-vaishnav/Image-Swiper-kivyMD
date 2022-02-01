d78742d8f251df79e0ed1abd0215f732bbf0b045
G1 <jeevanvaishnavv@gmail.com>
2022-02-01 19:37:36 +0530

    first commit

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ class ImageSwaper(MDApp):
         self.theme_cls.primary_palette = "BlueGray"
         return Builder.load_file('main.kv')
 
+    # Swipe Left
+    def on_swipe_left(self):
+        self.root.ids.toolbar.title = "Swipe Left"
 
-# IF name main
+    # Swipe Right
+
+    def on_swipe_right(self):
+        self.root.ids.toolbar.title = "Swipe Right"
+
+
+        # IF name main
 ImageSwaper().run()
